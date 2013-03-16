cf109ee1237718f904c17e36587bab85d784af8f
Michael <mike.sabat@gmail.com>
2013-03-16 14:00:06 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnePercent::Application.config.secret_token = '691854482eb082e29f178cda37e29aa8abc1023b69b7dd169c1cd175b0a6b9b18405241b8a6a1ed1b7de2bc93ed25f61006e257a28fe89f3689717016242ad20'
